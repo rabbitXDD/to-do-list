45fadb6f4eb3fdb80a1bade747ceb6f5a290ad98
David <rockmanx41024@yahoo.com.tw>
2015-04-29 15:41:19 +0800

    fff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429070215) do
+ActiveRecord::Schema.define(version: 20150429073842) do
 
   create_table "finishes", force: :cascade do |t|
     t.integer  "topic_id"
